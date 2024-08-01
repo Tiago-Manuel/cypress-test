38fbb89fbc5219a6ef83484e49151f91c64b980c
Tiago-Manuel <88378655+Tiago-Manuel@users.noreply.github.com>
2024-08-01 11:34:16 +0100

    add tests

--- angular/cypress/e2e/spec.cy.ts
+++ angular/cypress/e2e/spec.cy.ts
@@ -3,3 +3,16 @@ describe('template spec', () => {
     cy.visit('https://example.cypress.io')
   })
 })
+
+describe('Home Page Interaction', () => {
+  it('should navigate home, click the button, and verify the result in the div', () => {
+    // Navigate to the home page
+    cy.visit('http://localhost:4200');
+
+    // Click the button with the specified ID
+    cy.get('#weather').click();
+
+    // Expect the result in the div to be equal to the specified value
+    cy.get('#number').should('have.text', '25');
+  });
+});
--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular';
+  weather: any;
+  number: string = "";
+  constructor(private http: HttpClient) {
+
+  }
+
+
+  ngOnInit(): void {
+    console.log("i init")
+  }
+
+  getWeather() {
+    this.http.get('https://localhost:7019/WeatherForecast/Get').subscribe(res => {
+      this.weather = res;
+    })
+
+    this.http.get('https://localhost:7019/WeatherForecast/GetNumber').subscribe( data => {
+      console.log(data.toString())
+      this.number = data.toString();
+    })
+  }
 }
 
 "string hello on etwo"
